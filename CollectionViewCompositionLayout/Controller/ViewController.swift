5c467c43c75e2272ca13029c9596fa1cb605de4a
Daniil Sivozhelezov <sivozelezovdaniil@gmail.com>
2024-07-14 17:43:04 +0500

    Add model for collectionView

--- CollectionViewCompositionLayout/Controller/ViewController.swift
+++ CollectionViewCompositionLayout/Controller/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 final class ViewController: UIViewController {
 
     private lazy var collectionView: UICollectionView = {
-        let collectionView = UICollectionView(frame: view.frame, collectionViewLayout: UICollectionViewFlowLayout())
+        let collectionView = UICollectionView(frame: view.frame, collectionViewLayout: createLayout())
+        collectionView.dataSource = self
+        
+        collectionView.register(StoryCollectionViewCell.self, forCellWithReuseIdentifier: StoryCollectionViewCell.reuseId)
+        
+        collectionView.register(MessageCollectionViewCell.self, forCellWithReuseIdentifier: MessageCollectionViewCell.reuseId)
+        
+        collectionView.register(NewsCollectionViewCell.self, forCellWithReuseIdentifier: NewsCollectionViewCell.reuseId)
+        
+        collectionView.register(BannersCollectionViewCell.self, forCellWithReuseIdentifier: BannersCollectionViewCell.reuseId)
+        
         return collectionView
     }()
+    
+    private let collectionData = CollectionSection.mockData()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,5 +32,97 @@ final class ViewController: UIViewController {
         view.backgroundColor = .white
         view.addSubview(collectionView)
     }
+    
+    private func createLayout() -> UICollectionViewCompositionalLayout {
+        UICollectionViewCompositionalLayout { section, _ in
+            switch section {
+            case 0: self.createStorySection()
+//            case 1: self.createMessageSection()
+//            case 2: self.createNewsSection()
+            default: self.createMessageSection()
+            }
+        }
+    }
+    
+    private func createStorySection() -> NSCollectionLayoutSection {
+        // Item
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                              heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        // Group
+        let groupSize = NSCollectionLayoutSize(widthDimension: .absolute(100),
+                                               heightDimension: .estimated(104))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, repeatingSubitem: item, count: 1)
+        group.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 20)
+        
+        // Section
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .continuous
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 30, bottom: 40, trailing: 30)
+        
+        return section
+    }
+    
+    private func createMessageSection() -> NSCollectionLayoutSection {
+        // Item
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                              heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 10, trailing: 0)
+        
+        // Group
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.85),
+                                               heightDimension: .estimated(90))
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, repeatingSubitem: item, count: 2)
+        group.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 20)
+        
+        // Section
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .groupPagingCentered
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 30, bottom: 40, trailing: 30)
+        
+        return section
+    }
+//    
+//    private func createNewsSection() -> NSCollectionLayoutSection {
+//        
+//    }
+//    
+//    private func createBannerSection() -> NSCollectionLayoutSection {
+//        
+//    }
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        collectionData[section].items.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let item = collectionData[indexPath.section].items[indexPath.row]
+        
+        switch indexPath.section {
+        case 0:
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: StoryCollectionViewCell.reuseId, for: indexPath) as? StoryCollectionViewCell else { return UICollectionViewCell() }
+            cell.setupCell(item: item)
+            return cell
+        case 1:
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MessageCollectionViewCell.reuseId, for: indexPath) as? MessageCollectionViewCell else { return UICollectionViewCell() }
+            cell.setupCell(item: item)
+            return cell
+        case 2:
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: NewsCollectionViewCell.reuseId, for: indexPath) as? NewsCollectionViewCell else { return UICollectionViewCell() }
+            cell.setupCell(item: item)
+            return cell
+        default:
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: BannersCollectionViewCell.reuseId, for: indexPath) as? BannersCollectionViewCell else { return UICollectionViewCell() }
+            cell.setupCell(item: item)
+            return cell
+        }
+    }
+}
